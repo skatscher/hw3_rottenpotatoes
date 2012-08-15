ca1c4e48342b3459d1c3eb4eaae4e1843ab00b08
Stefan Katscher <skatscher@gmail.com>
2012-08-15 14:50:39 -0700

    implement part 2a: scenario 'restrict to movies with PG or R rating'

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -25,4 +25,12 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split.each do |rating|
+    sanitized = rating.gsub("\"", "").gsub(",", "")
+    if uncheck == "un"
+      uncheck("ratings_#{sanitized}")
+    else
+      check("ratings_#{sanitized}")
+    end
+  end
 end
